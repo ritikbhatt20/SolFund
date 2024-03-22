6e3f15415c4126555ccc9e63794ffafdf5b60be4
ritik <ritikbhatt020@gmail.com>
2024-03-23 03:19:39 +0530

    🎯Added errors

--- src/errors.rs
+++ src/errors.rs
@@ -4,4 +4,8 @@ use anchor_lang::prelude::error_code;
 pub enum ProjectError {
     #[msg("Funds Already Been Claimed")]
     FundsAlreadyClaimed,
+    #[msg("Cannot Claim As You Are Not The Owner")]
+    UnauthorizedToClaim,
+    #[msg("Funds Raised Less Than Funding Goal")]
+    FundingGoalNotReached,
 }
